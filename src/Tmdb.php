f89af40ff98749e2bff2a01680eeadd44d54475a
Joshua Aryadi <57905453+josh996@users.noreply.github.com>
2022-02-11 22:27:32 +0700

    Update Tmdb.php

--- src/Tmdb.php
+++ src/Tmdb.php
@@ -6,8 +6,24 @@ use GuzzleHttp\Client;
 
 class Tmdb
 {
+    const ENDPOINT = "https://api.themoviedb.org/3";
+    const MOVIE = 'movie';
+    const TV = 'tv';
+
     public function __construct()
     {
         # code...
+    }
+
+    public function getData($method = "GET", $type, $opt = []) 
+    {
+        $client = new Client([
+            'base_uri' => self::ENDPOINT,
+            'timeout'  => 2,
+        ]);
+
+        $request = $client->request($method, $type, $opt);
+
+        dd($request);
     }
 }

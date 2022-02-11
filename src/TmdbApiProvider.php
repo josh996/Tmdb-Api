ef9296931f1f63cbf0bc3587e9f8f5775e557993
Joshua Aryadi <57905453+josh996@users.noreply.github.com>
2022-02-11 12:06:31 +0700

    Update TmdbApiProvider.php

--- src/TmdbApiProvider.php
+++ src/TmdbApiProvider.php
@@ -6,5 +6,17 @@ use Illuminate\Support\ServiceProvider;
 
 class TmdbApiProvider extends ServiceProvider
 {
-    
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/tmdb.php' => config_path('tmdb.php'),
+        ]);
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/tmdb.php', 'tmdb'
+        );
+    }
 }

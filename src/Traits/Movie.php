a780be4605fdf73c9b4633af98a3254a05d65bff
Joshua Aryadi <57905453+josh996@users.noreply.github.com>
2022-02-12 22:40:08 +0700

    update

--- src/Traits/Movie.php
+++ src/Traits/Movie.php
@@ -18,4 +18,20 @@ trait Movie
     {
         return $this->getData("GET", self::MOVIE."/".$id."/credits", $opt);
     }
+
+    public function getExternalID($id, $opt =[])
+    {
+        return $this->getData("GET", self::MOVIE."/".$id."/external_ids", $opt);
+    }
+
+    public function getImages($id, $opt =[])
+    {
+        return $this->getData("GET", self::MOVIE."/".$id."/images", $opt);
+    }
+
+    public function getKeywords($id, $opt =[])
+    {
+        return $this->getData("GET", self::MOVIE."/".$id."/keywords", $opt);
+    }
+    
 }

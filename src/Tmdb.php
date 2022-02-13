45acedf94193cddba235755985de2d2eacec9c99
Joshua Aryadi <57905453+josh996@users.noreply.github.com>
2022-02-13 11:27:40 +0700

    Update Tmdb.php

--- src/Tmdb.php
+++ src/Tmdb.php
@@ -7,10 +7,12 @@ use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Psr7\Message;
 use Josh996\TmdbApi\Traits\Movie;
 use Josh996\TmdbApi\Traits\Tv;
+use Josh996\TmdbApi\Traits\TvEpisode;
+use Josh996\TmdbApi\Traits\TvSeasson;
 
 class Tmdb
 {
-    use Movie, Tv;
+    use Movie, Tv, TvSeasson, TvEpisode;
     
     const ENDPOINT = "https://api.themoviedb.org/";
     const MOVIE = '/movie';
@@ -24,7 +26,7 @@ class Tmdb
 
     }
 
-    public function getData($method = "GET", $type, $opt = []) 
+    public function getData($method = "GET", $url, $opt = []) 
     {
         $query = [
             "query" => [
@@ -32,13 +34,25 @@ class Tmdb
                 'language' => config('tmdb.language')
             ]
         ];
+
+        $url = $url;
+        if (is_array($url)) {
+            $converUrl = '';
+            for ($i=0; $i < count($url); $i++) {
+                $converUrl .= $url.'/';
+            }
+            $url = ltrim($converUrl, '/');
+        }
+
+        dump($url);
+        
         $client = new Client([
             'base_uri' => self::ENDPOINT,
             'timeout'  => 2,
         ]);
 
         try {
-            $request = $client->request($method, self::VERSION.$type, array_merge_recursive($query, $opt));
+            $request = $client->request($method, self::VERSION.$url, array_merge_recursive($query, $opt));
             $data = json_decode($request->getBody(), $this->isArray);
         } catch (ClientException $e) {
             $data = json_decode(Message::bodySummary($e->getResponse()), $this->isArray);

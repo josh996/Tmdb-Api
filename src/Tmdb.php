fdf5948465ebd94952f487224973f5b3ff23c55f
Joshua Aryadi <57905453+josh996@users.noreply.github.com>
2022-02-13 13:01:06 +0700

    Update Tmdb.php

--- src/Tmdb.php
+++ src/Tmdb.php
@@ -4,13 +4,9 @@ namespace Josh996\TmdbApi;
 
 class Tmdb
 {
-    public function __construct()
-    {
+    public function __construct() { }
 
-    }
+    public function movie($id) { return new Movie($id); }
 
-    public function movie($id)
-    {
-        return new Movie($id);
-    }
+    public function tv($id) { return new Tv($id); }
 }

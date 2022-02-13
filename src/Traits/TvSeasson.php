645971ef71e08cb1ec77fbbbd471a7b8dc2d8f63
Joshua Aryadi <57905453+josh996@users.noreply.github.com>
2022-02-13 11:27:49 +0700

    Create TvSeasson.php

---
+++ src/Traits/TvSeasson.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Josh996\TmdbApi\Traits;
+
+trait TvSeasson
+{
+    public function getTv($id, $seasson, $opt = [])
+    {
+        $append = [
+            "query" => [
+                'append_to_response' => config('tmdb.append_to_response')
+            ]
+        ];
+
+        return $this->getData("GET", self::TV."/".$id."/season/".$seasson, array_merge_recursive($append, $opt));
+    }
+}

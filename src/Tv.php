f4327ab63b2b4283db05568f0aafc5ed9e3d6079
Joshua Aryadi <57905453+josh996@users.noreply.github.com>
2022-02-13 13:01:11 +0700

    Create Tv.php

---
+++ src/Tv.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace Josh996\TmdbApi;
+
+class Tv extends GetData
+{
+    public $id;
+
+    public function __construct($id)
+    {
+        $this->id = $id;
+    }
+
+    public function get($options = []) {
+        $append = [
+            "query" => [
+                'append_to_response' => config('tmdb.append_to_response')
+            ]
+        ];
+
+        $data = $this->request(
+            'GET', 
+            '/3/tv/'.$this->id, 
+            array_merge_recursive($append, $options)
+        );
+
+        return json_decode($data, $this->isArray);
+    }
+}

a4eb3b13809fbaa6a28ad9e28b1d632a7b6f7945
Joshua Aryadi <57905453+josh996@users.noreply.github.com>
2022-02-14 03:36:06 +0700

    Create Season.php

---
+++ src/Season.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Josh996\TmdbApi;
+
+class Season extends GetData
+{
+    public $id, $season;
+
+    public function __construct($id, $season = 0)
+    {
+        $this->id = $id;
+        $this->season = $season;
+    }
+
+    public function get($options = []) {
+        $append = [
+            "query" => [
+                'append_to_response' => config('tmdb.append_to_response')
+            ]
+        ];
+
+        $data = $this->request(
+            'GET', 
+            '/3/tv/' . $this->id . '/season/' . $this->season, 
+            array_merge_recursive($append, $options)
+        );
+
+        return json_decode($data, $this->isArray);
+    }
+}
